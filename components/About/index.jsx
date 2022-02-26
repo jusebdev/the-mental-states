7a5ee80a757be51acc29939f925eba0fe949a487
outsiderintrospector <juansebcarv@outlook.com>
2022-02-26 17:07:06 -0300

    Initial commit

---
+++ components/About/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+
+export default function About (){
+return <section className='about-section'></section>
+
+}
---
+++ components/Featured/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Featured() {
+  return (
+<section className='Featured-section'> </section>
+  )
+}
+
+export default Featured
